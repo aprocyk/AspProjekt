7d2e2b0f881d9a385405ee618175f6e10ae46373
aprocyk <48320987+aprocyk@users.noreply.github.com>
2020-10-08 10:40:25 +0200

    Dodaj pliki projektów.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspProjekt.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string StringValue()
+        {
+            return "Hello World";
+        }
+
+        public IActionResult RedirectToGoogle()
+        {
+            return Redirect("http://www.google.com");
+        }
+
+        public IActionResult GetJson()
+        {
+            return Json(new {Name = "Jan", Surname = "Kowalski" });
+        }
+        public IActionResult TestModel()
+        {
+            var model = new List<TestModel>() {
+                new TestModel {},
+                new TestModel {},
+                new TestModel {}
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspProjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
